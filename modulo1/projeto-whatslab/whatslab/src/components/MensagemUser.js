b0c6043c2bc101f520f0bcb5f089b5f98e335e1f
Davi Emmanuel <davi.emannuellre@gmail.com>
2022-06-05 10:46:50 -0300

    fix: corrigindo bugs de responsividade

--- modulo1/projeto-whatslab/whatslab/src/components/MensagemUser.js
+++ modulo1/projeto-whatslab/whatslab/src/components/MensagemUser.js
@@ -2,45 +2,34 @@ import react from "react";
 import styled from "styled-components";
 
 const AreaMsg = styled.div`
-  width: 100%;
+  width: 10%;
   display: flex;
-  .userMsg {
-    justify-content: flex-end;
-  }
+  justify-content: flex-end;
 `
 
 const MsgDiv = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: left;
-  margin-left: 10px;
+  align-items: flex-start;
+  margin-right: 10px;
   margin-bottom: 5px;
-  background-color: white;
+  background-color: #dcf8c6;
   width: max-content;
-  padding: 5px 15px;
+  padding: 10px 10px;
   border-radius: 10px;
-`
-const User = styled.p`
-  font-weight: bold;
-  margin: 5px;
-  color:  purple;
-  /* &:after {
-    content: ":";
-  } */
 `
 const Msg = styled.p`
   margin: 0 5px;
 `
 
-function Mensagem(props) {
+function MensagemUser(props) {
   return (
     <AreaMsg>
       <MsgDiv>
-          <User>{props.user}</User>
           <Msg>{props.msg}</Msg>
       </MsgDiv>
     </AreaMsg>
   );
 }
 
-export default Mensagem;
+export default MensagemUser;
