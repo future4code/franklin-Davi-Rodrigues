931c01c78ab10588c8f772a24db07a4cbc553180
Davi Emmanuel <davi.emannuellre@gmail.com>
2022-09-07 10:50:41 -0300

    restante dos exercícios

--- modulo3/introducao-autenticacao/src/generateToken.ts
+++ modulo3/introducao-autenticacao/src/generateToken.ts
@@ -3,7 +3,7 @@ import * as jwt from "jsonwebtoken";
 import dotenv from 'dotenv'
 dotenv.config()
 
-const expiresIn = "1min"
+const expiresIn = "10min"
 
 export const generateToken = (input: AuthenticationData): string => {
   const token = jwt.sign(
--- modulo3/introducao-autenticacao/src/index.ts
+++ modulo3/introducao-autenticacao/src/index.ts
@@ -1,6 +1,9 @@
 import app from "./app"
 import { createUser } from "./createUser";
+import { findUserByEmail } from "./findUserByEmail";
 import { AuthenticationData, generateToken } from "./generateToken";
+import { getDataOfToken } from "./getDataOfToken";
+import { getUserById } from "./getUserById";
 // import editUser from './endpoints/editUser'
 // import createUser from './endpoints/createUser'
 
@@ -34,3 +37,40 @@ app.post('/user/signup',async (req, res) => {
         console.log(erro);
     }
 })
+
+app.post('/user/login',async (req, res) => {
+    try {
+        const {email, password} = req.body
+        if(!email || email.indexOf('@') === -1){
+            throw new Error('Email inválido')
+        }
+        
+        const user = await findUserByEmail(email)
+
+        if(user.password !== password){
+            throw new Error("Sua senha não é essa não, meu parceiro")
+        }
+
+        const authentication : AuthenticationData = {id: user.id}
+        const token = generateToken(authentication)
+
+        res.send(token)
+    }catch(erro){
+        console.log(erro);
+    }
+})
+
+
+app.get('/user/profile', async (req, res) => {
+    const token = req.headers.authorization as string
+
+    const { id } = getDataOfToken(token)
+    const { email } = await getUserById(id)
+
+    res.send({
+        "id": id,
+        "email": email
+    })
+
+})
+
